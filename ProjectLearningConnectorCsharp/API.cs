d6d14bedf30b6eed74d776b7e9d6cdfa18c28c7c
Hannes Samskog <hannessamskog@gmail.com>
2021-01-23 14:19:57 +0100

    Add project files.

---
+++ ProjectLearningConnectorCsharp/API.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLearningConnectorCsharp
+{
+    class API
+    {
+        private const string BasePath = "LocalHost";
+        private readonly HttpClient _client = new HttpClient { BaseAddress = new Uri(BasePath) };
+
+        public API(string apiKey)
+        {
+            _client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+        }
+
+        public async Task<State> NewGame(string map)
+        {
+            var data = new StringContent(map, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("new", data);
+
+            var result = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("Exception:" + result);
+                Console.WriteLine();
+                Console.WriteLine("Fatal Error: could not start a new game");
+                Environment.Exit(1);
+            }
+
+            return JsonConvert.DeserializeObject<State>(result);
+        }
+    }
+}
---
+++ ProjectLearningConnectorCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjectLearningConnectorCsharp
+{
+    class Program
+    {
+        //Enter your ApiKey
+        private const string ApiKey = "777532";
+
+        //Pick your task
+        private const string Task = "task1";     
+        static void Main(string[] args)
+        {
+              
+        }
+    }
+}
